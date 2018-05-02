c7bdb1003641a091b10e933020c736bf2cfd60ef
Mags12 <33438630+Mags12@users.noreply.github.com>
2018-05-02 19:25:30 -0400

    Spotify Player

---
+++ Spotify/public/scripts/click-event.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('body').on('click', '.track', function(){
+        const _this = $(this);
+        const id = _this.data('id');
+        const result = $.ajax({
+            url: `/track/${id}`
+        });
+        
+    })
+})
