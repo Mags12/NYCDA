21051e25b87a7ec94ca99d72f50e62323438da46
Mags12 <33438630+Mags12@users.noreply.github.com>
2018-05-02 19:31:33 -0400

    Bulletin Board

---
+++ tweedr/client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Tweeder from './components/tweedr';
+
+
+ReactDOM.render(<Tweeder />, document.getElementById('root'));
