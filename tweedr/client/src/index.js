4dae2188f7fec831d9b70aeb3e1f61f0f4e4464c
Mags12 <33438630+Mags12@users.noreply.github.com>
2018-05-06 17:49:17 -0400

    Delete index.js

--- tweedr/client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Tweeder from './components/tweedr';
-
-
-ReactDOM.render(<Tweeder />, document.getElementById('root'));
